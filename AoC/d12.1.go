90d82fd80ad17c09768bc5a4a05b12c9b239bc65
ge-hall <ge-hall@outlook.com>
2021-12-22 17:20:27 +1000

    Day 12 1 & 2 complete

--- AoC/d12.1.go
+++ AoC/d12.1.go
@@ -7,6 +7,8 @@ import (
 	"strings"
 )
 
+var pathCount int = 0
+
 func D12_1() {
 	dat, err := ioutil.ReadFile("inputd12")
 	if err != nil {
@@ -23,12 +25,14 @@ func D12_1() {
 
 		// add connection
 		cs.ConnectCave(cs.AddCave(caves[0]), cs.AddCave(caves[1]))
+		cs.ConnectCave(cs.AddCave(caves[1]), cs.AddCave(caves[0]))
 
 	}
 	print("===============\n\n")
 	cs.String()
 	print("===============\n\n")
 	cs.Traverse()
+	print(pathCount)
 }
 
 type CaveSystem struct {
@@ -61,19 +65,23 @@ func (cs *CaveSystem) ConnectCave(c1, c2 *Cave) {
 		cs.connections = make(map[Cave][]*Cave)
 	}
 	cs.connections[*c1] = append(cs.connections[*c1], c2)
+	//if c1.big {
+	//	cs.connections[*c2] = append(cs.connections[*c2], c1)
+	//}
 }
 
 func (cs *CaveSystem) String() {
 	s := ""
 	for i := 0; i < len(cs.caves); i++ {
 		fmt.Printf("cave:%o\n", cs.caves[i])
-		if !cs.caves[i].entrance {
-			continue
-		}
+		//if !cs.caves[i].entrance {
+		//	continue
+		//}
 		s += cs.caves[i].String() + " -> "
 		near := cs.connections[*cs.caves[i]]
 		for j := 0; j < len(near); j++ {
 			s += near[j].String() + " "
+			fmt.Printf("connections:%o\n", near[j])
 		}
 		s += "\n"
 	}
@@ -88,19 +96,52 @@ func (cs *CaveSystem) Traverse() {
 			continue
 		}
 		var path []string
+
 		cs.FindPaths(*cs.caves[i], path)
 
 	}
 }
 func (cs *CaveSystem) FindPaths(cave Cave, path []string) {
 	path = append(path, cave.label)
-	if cave.exit {
+	var caveCount = 0
+	var revisited map[string]int = make(map[string]int)
+	var revisitCount int = 0
+	for i := 0; i < len(path); i++ {
+		if path[i] == cave.label {
+			caveCount++
+		}
+		if strings.ToLower(path[i]) == path[i] {
 
-		fmt.Printf(",%s", path)
+			for j := 0; j < len(path); j++ {
+				if path[i] == path[j] {
+					revisited[path[i]]++
+				}
+			}
+
+		}
+	}
+	for _, v := range revisited {
+		if v > 1 {
+			revisitCount++
+		}
+	}
+	//fmt.Printf("||%o %d %d\n", path, revisitCount, caveCount)
+	//fmt.Printf("%s\n", path)
+	if cave.exit {
+		pathCount++
+		fmt.Printf("%s %d %d\n", path, revisitCount, caveCount)
+		return
+	}
+
+	if (caveCount > 2 && !cave.big) || revisitCount > 1 {
+		//fmt.Printf("||%o %d %d\n", path, revisitCount, caveCount)
 		return
 	}
 
 	for c := 0; c < len(cs.connections[cave]); c++ {
+		if cs.connections[cave][c].entrance {
+			continue
+		} // do not go back to start
 		cs.FindPaths(*cs.connections[cave][c], path)
 	}
 }
