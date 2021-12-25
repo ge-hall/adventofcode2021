be4e6f040b4fe8b4db9f18319cbdba28d347a909
ge-hall <ge-hall@outlook.com>
2021-12-25 20:08:50 +1000

    Day 13 wip

--- AoC/d13.1.go
+++ AoC/d13.1.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"io/ioutil"
 	"log"
+	"strconv"
 	"strings"
 )
 
@@ -15,14 +16,40 @@ func D13_1() {
 
 	var data = strings.Split(string(dat), "\n")
 	//var paper [][]int
-	//var plotting = true
+	var plotting = true
+	var x []int
+	xMax := 0
+	var y []int
+	yMax := 0
 	for _, line := range data {
 		fmt.Printf("%s\n", line)
 		if line == "" {
-			print("empty line")
+			print("empty line\n")
+			plotting = false
+			continue
+		}
+		if !plotting {
+			// read instructions
+			continue
 		}
 
+		coord := strings.Split(line, ",")
+		xv, _ := strconv.ParseInt(coord[0], 10, 32)
+		if int(xv) > xMax {
+			xMax = int(xv)
+		}
+		x = append(x, int(xv))
+
+		yv, _ := strconv.ParseInt(coord[1], 10, 32)
+		if int(yv) > yMax {
+			yMax = int(yv)
+		}
+		y = append(y, int(yv))
+
 	}
+
+	fmt.Printf("%v max = %d\n", x, xMax)
+	fmt.Printf("%v max = %d\n", y, yMax)
 	print("===============\n\n")
 	print("===============\n\n")
 	print(pathCount)
