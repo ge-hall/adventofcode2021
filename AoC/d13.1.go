22c17db3f26d8e060ff8abbf40c2ddd2c9840fdc
ge-hall <ge-hall@outlook.com>
2021-12-27 21:42:42 +1000

    Day 13 Complete

--- AoC/d13.1.go
+++ AoC/d13.1.go
@@ -8,6 +8,11 @@ import (
 	"strings"
 )
 
+type Fold struct {
+	direction string
+	distance  int
+}
+
 func D13_1() {
 	dat, err := ioutil.ReadFile("inputd13")
 	if err != nil {
@@ -15,7 +20,7 @@ func D13_1() {
 	}
 
 	var data = strings.Split(string(dat), "\n")
-	//var paper [][]int
+	var folds []Fold
 	var plotting = true
 	var x []int
 	xMax := 0
@@ -30,6 +35,10 @@ func D13_1() {
 		}
 		if !plotting {
 			// read instructions
+			instr := strings.Split(line, " ")
+			fold := strings.Split(instr[2], "=")
+			distance, _ := strconv.ParseInt(fold[1], 10, 32)
+			folds = append(folds, Fold{fold[0], int(distance)})
 			continue
 		}
 
@@ -47,10 +56,98 @@ func D13_1() {
 		y = append(y, int(yv))
 
 	}
+	paper := make([][]string, yMax+1)
+	// add dimensions
+	for y := 0; y <= yMax; y++ {
+		paper[y] = make([]string, xMax+1)
+	}
+	for _, r := range paper {
+		for i, _ := range r {
+			r[i] = " "
+		}
+	}
+	// print plotting
+	for i := 0; i < len(x); i++ {
+		paper[y[i]][x[i]] = "#"
+	}
+	//fmt.Printf("%v\n", paper)
+	fmt.Printf("%d max = %d\n", yMax, xMax)
 
-	fmt.Printf("%v max = %d\n", x, xMax)
-	fmt.Printf("%v max = %d\n", y, yMax)
+	for i, _ := range folds {
+		for j, r := range paper {
+			for k, c := range r {
+				if folds[i].direction == "y" && j == folds[i].distance {
+					print("- ")
+				} else if folds[i].direction == "x" && k == folds[i].distance {
+					print("|")
+				} else {
+					fmt.Printf("%s ", c)
+				}
+			}
+			print("\n")
+		}
+		fold(paper, folds[i].distance, folds[i].direction)
+		if folds[i].direction == "y" {
+			paper = paper[:folds[i].distance]
+		} else {
+			for r, _ := range paper {
+				paper[r] = paper[r][:folds[i].distance]
+			}
+		}
+		// count plots
+		var count int = 0
+		for _, col := range paper {
+			for _, p := range col {
+				if p == "#" {
+					count++
+				}
+			}
+		}
+		fmt.Printf("count = %d\n", count)
+	}
+	// count plots
+	var count int = 0
+	for _, col := range paper {
+		for _, p := range col {
+			if p == "#" {
+				count++
+			}
+		}
+	}
+	for _, r := range paper {
+		for _, c := range r {
+			fmt.Printf("%s ", c)
+		}
+		print("\n")
+	}
+	//fmt.Printf("%v\n", paper)
+	//fmt.Printf("%v fold = %d\n", x, folds[0])
+	//fmt.Printf("%v max = %d\n", y, yMax)
 	print("===============\n\n")
 	print("===============\n\n")
-	print(pathCount)
+	print(count)
+}
+func fold(paper [][]string, f int, dir string) {
+	if dir == "y" {
+		for fa, fb := 0, 1; fa < f; {
+			for x := 0; x < len(paper[fa]); x++ {
+				if paper[len(paper)-fb][x] == "#" {
+					paper[fa][x] = paper[len(paper)-fb][x]
+				}
+			}
+			fa++
+			fb++
+		}
+	} else {
+		for fa, fb := 0, 1; fa < f; {
+			for y := 0; y < len(paper); y++ {
+				if paper[y][len(paper[y])-fb] == "#" {
+					paper[y][fa] = paper[y][len(paper[y])-fb]
+				}
+			}
+			fa++
+			fb++
+		}
+	}
+
 }
