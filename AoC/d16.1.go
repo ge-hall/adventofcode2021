3dd6eb112726cab2a23cf844c75cf78c5e6517b6
ge-hall <ge-hall@outlook.com>
2022-01-07 19:12:20 +1000

    Day 16 1 process each grab with a function

--- AoC/d16.1.go
+++ AoC/d16.1.go
@@ -20,40 +20,78 @@ func D16_1() {
 	for _, line := range data {
 		fmt.Printf("%s\n", line)
 		dst := make([]byte, hex.DecodedLen(len(line)))
-		hex.Decode(dst, []byte(line))
+		numBytes, _ := hex.Decode(dst, []byte(line))
+		fmt.Printf("length of array is %d\n", numBytes)
+		fmt.Printf("array is %v\n", dst)
 		// dst is byte array
-		dst = append(dst, uint8(0))
-		b := binary.BigEndian.Uint32(dst[0:])
-
-		fmt.Printf("%b\n", b)
-		var version uint8 = uint8(b >> 29)
-		var typeID uint8 = uint8(b << 3 >> 29)
-		var aGroup uint8 = uint8(b << 5 >> 26)
-		var bGroup uint8 = uint8(b << 11 >> 27)
-		var cGroup uint8 = uint8(b << 16 >> 27)
-		fmt.Printf("VVV=%b\n", version)
-		fmt.Printf("TTT=%b\n", typeID)
-		fmt.Printf("AAAAA=%08b\n", aGroup)
-		fmt.Printf("BBBBB=%08b\n", bGroup)
-		fmt.Printf("CCCCC=%08b\n", cGroup)
-		// need to construct binary value of 15 bits by building 5 bits at a time and shift left
-		// or bitwise & with mask
-		//v := binary.BigEndian.Uint32([]byte{0, aGroup, bGroup, cGroup})o
-		var v1 uint16 = uint16(aGroup) << 9
-		var v2 uint16 = uint16(bGroup) << 4
-		var v uint16 = v1 | v2 | uint16(cGroup)
-		if typeID == 4 {
-			fmt.Printf("Type 4 literal value composed of last 4 bits of each group\n")
-			v1 = uint16(aGroup<<4) << 4
-			v2 = uint16(bGroup << 4)
-			v = v1 | v2 | uint16(cGroup)
+		if len(dst) < 4 {
+			dst = append(dst, uint8(0))
 		}
+		var bitPointer = 0
+		var bytePointer = 0
+		word := binary.BigEndian.Uint32(dst[bytePointer : bytePointer+4])
+		// get 3 bits
+		fmt.Printf("AAA:%03b\n", word)
 
-		fmt.Printf("v1 value= %015b => %d\n", v1, v1)
-		fmt.Printf("v2 value= %015b => %d\n", v2, v2)
-		fmt.Printf("value= %015b => %d\n", v, v)
+		// parse packets
+		versionSum := 0
+		for {
+			version := getNextBits(dst, 3, bitPointer)
+			versionSum += int(version)
+			bitPointer += 3
+			fmt.Printf("VVV:%03b\n", version)
+			typeID := getNextBits(dst, 3, bitPointer)
+			bitPointer += 3
+			fmt.Printf("TTT:%03b\n", typeID)
+			if typeID == 4 {
+
+			} else {
+				lengthType := getNextBits(dst, 1, bitPointer)
+				bitPointer += 1
+				fmt.Printf("lengthType:%b\n", lengthType)
+				var packetCount uint16
+				if lengthType == 0 {
+					packetCount = getNextBits(dst, 15, bitPointer)
+					bitPointer += 15
+					fmt.Printf(":packetCount:%b\n", packetCount)
+				} else {
+					packetCount = getNextBits(dst, 11, bitPointer)
+					bitPointer += 11
+					fmt.Printf(":packetCount:%b\n", packetCount)
+				}
+				for i := 0; i < int(packetCount); i++ {
+					packet := getNextBits(dst, 11, bitPointer)
+					bitPointer += 11
+					fmt.Printf(":packet:%b\n", packet)
+				}
+
+			}
+			if len(dst)*8-bitPointer < 8 {
+				break
+			}
+
+		}
+		fmt.Printf("result = %d", versionSum)
 
 	}
-	// implement Dijkstra
+
+}
+
+func getNextBits(bytes []byte, bits int, bitPtr int) uint16 {
+	fmt.Printf("%v, %d, %d[%08b]\n", bytes, bits, bitPtr, bytes[bitPtr/8])
+	var result uint16
+	// for each bit
+	for b := 0; b < bits; b++ {
+		// get currentByte and shift right
+		currByte := bytes[bitPtr/8] >> (7 - bitPtr%8)
+		//>> (8 - bitPtr%8)
+		fmt.Printf("currByte:%08b - %d\n", currByte, 7-bitPtr%8)
+		result = result << 1
+		result = result | uint16(currByte&1)
+
+		bitPtr++
+
+	}
+	return result
 
 }
