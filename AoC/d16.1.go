6d3ee05fbe1ed49ea4b22923418094aed85c2b64
ge-hall <ge-hall@outlook.com>
2022-01-09 11:10:24 +1000

    Day 16 1 add readPacket function

--- AoC/d16.1.go
+++ AoC/d16.1.go
@@ -1,7 +1,6 @@
 package AoC
 
 import (
-	"encoding/binary"
 	"encoding/hex"
 	"fmt"
 	"io/ioutil"
@@ -22,64 +21,87 @@ func D16_1() {
 		dst := make([]byte, hex.DecodedLen(len(line)))
 		numBytes, _ := hex.Decode(dst, []byte(line))
 		fmt.Printf("length of array is %d\n", numBytes)
-		fmt.Printf("array is %v\n", dst)
+		fmt.Printf("array is %v\n\n", dst)
 		// dst is byte array
 		if len(dst) < 4 {
 			dst = append(dst, uint8(0))
 		}
 		var bitPointer = 0
-		var bytePointer = 0
-		word := binary.BigEndian.Uint32(dst[bytePointer : bytePointer+4])
-		// get 3 bits
-		fmt.Printf("AAA:%03b\n", word)
 
 		// parse packets
 		versionSum := 0
-		for {
-			version := getNextBits(dst, 3, bitPointer)
-			versionSum += int(version)
-			bitPointer += 3
-			fmt.Printf("VVV:%03b\n", version)
-			typeID := getNextBits(dst, 3, bitPointer)
-			bitPointer += 3
-			fmt.Printf("TTT:%03b\n", typeID)
-			if typeID == 4 {
+		readPacket(dst, bitPointer)
 
-			} else {
-				lengthType := getNextBits(dst, 1, bitPointer)
-				bitPointer += 1
-				fmt.Printf("lengthType:%b\n", lengthType)
-				var packetCount uint16
-				if lengthType == 0 {
-					packetCount = getNextBits(dst, 15, bitPointer)
-					bitPointer += 15
-					fmt.Printf(":packetCount:%b\n", packetCount)
-				} else {
-					packetCount = getNextBits(dst, 11, bitPointer)
-					bitPointer += 11
-					fmt.Printf(":packetCount:%b\n", packetCount)
-				}
-				for i := 0; i < int(packetCount); i++ {
-					packet := getNextBits(dst, 11, bitPointer)
-					bitPointer += 11
-					fmt.Printf(":packet:%b\n", packet)
-				}
-
-			}
-			if len(dst)*8-bitPointer < 8 {
-				break
-			}
-
-		}
 		fmt.Printf("result = %d", versionSum)
 
 	}
 
 }
+func readPacket(dst []byte, bitPtr int) int {
+	var packet int
+	versionSum := 0
+	version := getNextBits(dst, 3, bitPtr)
+	if version == 0 {
+		return 0
+	} // extra bits
+	versionSum += int(version)
+	bitPtr += 3
+	fmt.Printf("reading version %d packet.\n", version)
 
-func getNextBits(bytes []byte, bits int, bitPtr int) uint16 {
-	fmt.Printf("%v, %d, %d[%08b]\n", bytes, bits, bitPtr, bytes[bitPtr/8])
-	var result uint16
+	typeID := getNextBits(dst, 3, bitPtr)
+	bitPtr += 3
+	fmt.Printf("TTT:%03b\n", typeID)
+
+	if typeID == 4 {
+		for i := 0; i < 3; i++ {
+			packet = getNextBits(dst, 5, bitPtr)
+			bitPtr += 5
+			fmt.Printf(":packet:%b\n", packet)
+		}
+
+	} else {
+		lengthType := getNextBits(dst, 1, bitPtr)
+		bitPtr += 1
+		fmt.Printf("lengthType:%b\n", lengthType)
+		var packetCount int
+		var packetLength int = 11
+
+		if lengthType == 0 {
+			packetLength = getNextBits(dst, 15, bitPtr)
+			bitPtr += 15
+			fmt.Printf(":packetLength:%d\n\n\n", packetLength)
+			// read 11 bit chunks then remainder
+			for packetBits := 0; packetBits < packetLength; packetBits += 11 {
+				if packetLength-packetBits > 22 {
+					packet = getNextBits(dst, 11, bitPtr)
+					fmt.Printf(":--packet:%d\n", packet)
+					bitPtr += 11
+				} else {
+
+					packet = getNextBits(dst, packetLength-packetBits, bitPtr)
+					bitPtr += packetLength - packetBits
+					fmt.Printf(":--packet:%d\n", packet)
+					break
+				}
+			}
+		} else {
+			packetCount = getNextBits(dst, 11, bitPtr)
+			bitPtr += 11
+			fmt.Printf(":packetCount:%d\n\n\n", packetCount)
+			// packet count only applies to lengthType 1
+			for i := 0; i < int(packetCount); i++ {
+				packet = getNextBits(dst, 11, bitPtr)
+				bitPtr += 11
+				fmt.Printf(":packet:%d\n", packet)
+			}
+		}
+
+	}
+	return versionSum
+}
+func getNextBits(bytes []byte, bits int, bitPtr int) int {
+	fmt.Printf("%v, %d, %d[%08b]%d\n", bytes, bits, bitPtr/8, bytes[bitPtr/8], bytes[bitPtr/8])
+	var result int
 	// for each bit
 	for b := 0; b < bits; b++ {
 		// get currentByte and shift right
@@ -87,7 +109,7 @@ func getNextBits(bytes []byte, bits int, bitPtr int) uint16 {
 		//>> (8 - bitPtr%8)
 		fmt.Printf("currByte:%08b - %d\n", currByte, 7-bitPtr%8)
 		result = result << 1
-		result = result | uint16(currByte&1)
+		result = result | int(currByte&1)
 
 		bitPtr++
 
